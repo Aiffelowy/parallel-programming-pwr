673dd94c2ca4529b09f1d5db98c1739500c53b69
M1K45 <272570@student.pwr.edu.pl>
2025-10-15 15:10:25 +0200

    Dodanie zadania trzeciego (bez gwiazdki)

--- Kopalnia/Program.cs
+++ Kopalnia/Program.cs
@@ -1,4 +1,6 @@
-﻿class Mutex<T>(T value)
+﻿using System.Diagnostics;
+
+class Mutex<T>(T value)
 {
     private readonly object locker = new();
 
@@ -75,8 +77,8 @@ class Mine(int coal)
 
 class Worker(Mutex<WareHouse> warehouse_access, Semaphore<Mine> mine_access, int number)
 {
-    static readonly int TIME_TO_MINE = 3000;
-    static readonly int TIME_TO_UNLOAD = 1000;
+    static readonly int TIME_TO_MINE = 10;
+    static readonly int TIME_TO_UNLOAD = 10;
     static readonly int TRAVEL_TIME = 10000;
     public static readonly int CAPACITY = 200;
 
@@ -115,31 +117,84 @@ class Worker(Mutex<WareHouse> warehouse_access, Semaphore<Mine> mine_access, int
     }
 }
 
+class Solutions
+{
+    public static void thirdTask(int maxWorkers)
+    {
+        int[] time = new int[maxWorkers];
+        float[] acceleration = new float[maxWorkers];
+        float[] efficiency = new float[maxWorkers];
+
+
+        for (int i = 1; i <= maxWorkers; i++)
+        {
+            Mutex<WareHouse> warehouse = new(new WareHouse());
+            Semaphore<Mine> mine = new(new Mine(2000), 2);
+
+            Stopwatch timeOperation = Stopwatch.StartNew();
+            Task[] tasks = new Task[i];
+            for (int j = 0; j < i; j++)
+            {
+                var local_j = j;
+                tasks[j] = Task.Run(() => { var w = new Worker(warehouse, mine, local_j); w.work(); });
+            }
+            Cursor.print($"Simulation with {i} workers:", 0);
+            while (true)
+            {
+                using (var lock_ = warehouse.lock_())
+                {
+                    Cursor.print($"Coal in the warehouse: {(~lock_).coal_count}             ", 1);
+                }
+
+                using (var lock_ = mine.lock_())
+                {
+                    Cursor.print($"Coal left in the mine: {(~lock_).coal_count}            ", 2);
+                    if ((~lock_).coal_count < Worker.CAPACITY) { break; }
+                }
+            }
+
+            Task.WaitAll(tasks);
+            timeOperation.Stop();
+            time[i - 1] = (int)timeOperation.ElapsedMilliseconds;
+            acceleration[i - 1] = (float)time[0] / (float)time[i - 1];
+            efficiency[i - 1] = acceleration[i - 1] / i * 100;
+        }
+        Console.Clear();
+        for (int j = 0; j < maxWorkers; j++)
+        {
+            Console.WriteLine($"Workers: {j + 1}, Time: {time[j]} ms, Acceleration: {acceleration[j]:F2}, Efficiency: {efficiency[j]:F2} %\n");
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args) {
-        Mutex<WareHouse> warehouse = new(new WareHouse());
-        Semaphore<Mine> mine = new(new Mine(20000), 2);
-        Task[] tasks = new Task[5];
-        for (int i = 0; i < 5; i++) {
-            var local_i = i;
-            tasks[i] = Task.Run(() => { var w = new Worker(warehouse, mine, local_i); w.work(); });
-        }
+        Solutions.thirdTask(4);
+        //Mutex<WareHouse> warehouse = new(new WareHouse());
+        //Semaphore<Mine> mine = new(new Mine(20000), 2);
+        //Semaphore<Mine> secondMine = new(new Mine(20000), 2);
+        //Task[] tasks = new Task[5];
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    var local_i = i;
+        //    tasks[i] = Task.Run(() => { var w = new Worker(warehouse, mine, local_i); w.work(); });
+        //}
 
-        while(true)
-        {
-            using(var lock_ = warehouse.lock_())
-            {
-                Cursor.print($"Coal in the warehouse: {(~lock_).coal_count}             ", 0);
-            }
+        //while (true)
+        //{
+        //    using (var lock_ = warehouse.lock_())
+        //    {
+        //        Cursor.print($"Coal in the warehouse: {(~lock_).coal_count}             ", 0);
+        //    }
 
-            using(var lock_ =  mine.lock_())
-            {
-                Cursor.print($"Coal left in the mine: {(~lock_).coal_count }            ", 1);
-                if((~lock_).coal_count < Worker.CAPACITY) { break; }
-            }
-        }
+        //    using (var lock_ = mine.lock_())
+        //    {
+        //        Cursor.print($"Coal left in the mine: {(~lock_).coal_count}            ", 1);
+        //        if ((~lock_).coal_count < Worker.CAPACITY) { break; }
+        //    }
+        //}
 
-        Task.WaitAll(tasks);
+        //Task.WaitAll(tasks);
     }
 }
